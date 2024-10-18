5254149deda2d1f305205f85997a29f37c75a813
Shoyeb Ashkan <shoyebashkan999@gmail.com>
2024-10-18 20:09:54 +0530

    adding timeout to mongo db

--- config/db.js
+++ config/db.js
@@ -3,7 +3,12 @@ require("dotenv").config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGOOSE_URI);
+    await mongoose.connect(process.env.MONGOOSE_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 30000, // 30 seconds
+      socketTimeoutMS: 45000, // 45 seconds
+    });
     console.log("Connected to MongoDB");
 
     const connection = mongoose.connection;
